65a07238a976d199e88f66333a26eb095712616b
booniez <>
2024-04-22 11:22:03 +0800

    Modified files: .vuepress/config.ts

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -162,14 +162,14 @@ export default defineUserConfig({
   ],
   // 打开过后会导致 algolia 检索不到内容
   // 会默认给检索条件 form 里面加上 lang=zh-CN
-  // locales: {
-  //   // 键名是该语言所属的子路径
-  //   // 作为特例，默认语言可以使用 '/' 作为其路径。
-  //   '/': {
-  //     lang: 'zh-CN',
-  //     // title: 'VuePress',
-  //     // description: 'Vue 驱动的静态网站生成器',
-  //   },
-  // },
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'zh-CN',
+      // title: 'VuePress',
+      // description: 'Vue 驱动的静态网站生成器',
+    },
+  },
   // debug: true,
 });
